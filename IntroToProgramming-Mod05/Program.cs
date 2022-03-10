5b11a71d91a47ea1ae6fc1680d0c034a7ae2d534
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-10 15:54:41 -0500

    Updated logic.

--- IntroToProgramming-Mod05/Program.cs
+++ IntroToProgramming-Mod05/Program.cs
@@ -20,8 +20,10 @@ namespace DebugginAndExceptionHandling_CS
             course[] courseList = PopulateTranscript();
             double GPA = GetGPA(courseList);
             Console.WriteLine("Your GPA is currently: " + GPA);
+            Console.ReadLine();
         }
 
+        
         private static course[] PopulateTranscript()
         {
             course[] courseList = new course[5];
@@ -29,16 +31,13 @@ namespace DebugginAndExceptionHandling_CS
             for (int counter = 0; counter < courseList.Length; counter++)
             {
                 course newCourse = new course();
-                Console.WriteLine("Enter a course name");
 
-                newCourse.courseName = Console.ReadLine();
-                Console.WriteLine("Enter the credit hours for this course");
+                // Console.WriteLine("Enter a course name");
+                newCourse.courseName = GetInput("course name");
+                newCourse.creditHours = ConvertStringToInt(GetInput("credit hours"));
+                newCourse.gradePoints = ConvertStringToInt(GetInput("grade points"));
 
-                newCourse.creditHours = Console.ReadLine();
-                Console.WriteLine("Enter your grade points for this course");
-
-                newCourse.gradePoints = Console.ReadLine();
-                courseList[counter] = newCourse;
+                courseList[counter] = newCourse;               
 
             }
 
@@ -60,6 +59,48 @@ namespace DebugginAndExceptionHandling_CS
             result = totalGradePoints / totalCredHours;
 
             return result;
+        }
+
+        private static string GetInput(string input)
+        {
+            Console.WriteLine($"Enter the { input } for this course");
+            return Console.ReadLine();
+        }
+
+        private static int ConvertStringToInt(string input)
+        {
+            int result = 0;
+            
+            if (ValidateInteger(input))
+                result = Int32.Parse(input);
+
+            return result;
+        }
+        private static bool ValidateInteger(string input)
+        {
+            int convertFromString;
+            bool isValid = false;
+            // EX 1 Task 1.4:  Create a Try-Catch block to catch a FormatException exception  
+           
+                try
+                {
+                    convertFromString = Int32.Parse(input);
+                    isValid = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(e.Message);
+                    isValid = false;
+                }
+                // EX 1 Task 2.2:  Add a finally block to the exception handler, and then execute the garbage
+                // collector with GC.Collect().
+                finally
+                {
+                    GC.Collect();
+                }
+
+            return isValid;
         }
     }
 }
