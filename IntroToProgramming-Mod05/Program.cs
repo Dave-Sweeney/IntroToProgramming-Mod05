dc19ac58453ccc6c41ef467baa651ea2d2b8836c
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-10 14:26:02 -0500

    Add project files.

---
+++ IntroToProgramming-Mod05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToProgramming_Mod05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
